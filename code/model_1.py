054d87474fedd273f49951c238d9aaaa7e9bfa0f
crossley <matthewjohncrossley@gmail.com>
2023-08-11 10:20:00 +1000

    bayes stuff

--- code/model_1.py
+++ code/model_1.py
@@ -6,7 +6,6 @@ import os
 
 
 def simulate_model(params, *args):
-
     # simulation params
     n_trials = args[2]
     n_steps = 2000
@@ -27,9 +26,7 @@ def simulate_model(params, *args):
 
     for i in range(n_trials):
         for j in range(1, n_steps):
-
             if j > ndt:
-
                 # TODO: remind ourselves v_mean vs v_sd
                 new_evidence = np.random.normal(v_mean, v_sd)
                 evidence[i, j] = evidence[i, j - 1] + new_evidence
@@ -55,11 +52,11 @@ def simulate_model(params, *args):
 
 
 def obj_func(params, *args):
-    '''
+    """
     This function should accept a given set of parameters, simulate the model
     with those parameters, and then compare the result of that simulation with
     your objective (e.g., a person)
-    '''
+    """
 
     # simnulate the model
     r_choice_pred, rt_pred = simulate_model(params, *args)
@@ -68,8 +65,8 @@ def obj_func(params, *args):
     rt_obs = args[1] * 1000
 
     # NOTE: should cost factor in correct vs incorrect
-    sse_choice = (np.nanmean(r_choice_pred) - np.nanmean(r_choice_obs))**2
-    sse_rt = (np.nanmean(rt_pred) - np.nanmean(rt_obs))**2
+    sse_choice = (np.nanmean(r_choice_pred) - np.nanmean(r_choice_obs)) ** 2
+    sse_rt = (np.nanmean(rt_pred) - np.nanmean(rt_obs)) ** 2
 
     # NOTE: left in just to think about if we ever want trial order to matter
     # compare against objective
@@ -82,7 +79,6 @@ def obj_func(params, *args):
 
 
 def fit_model():
-
     v_mean = 0.1  # drift rate
     v_sd = 2.0  # drift rate
     a = 100.0  # threshold
@@ -91,20 +87,19 @@ def fit_model():
 
     params = (v_mean, v_sd, a, z, ndt)
 
-    dir_data = '../data_Stroop_PRmanipulation/'
-    d = pd.read_csv(dir_data + '2response_trimmed_combined.csv')
+    dir_data = "../data_Stroop_PRmanipulation/"
+    d = pd.read_csv(dir_data + "2response_trimmed_combined.csv")
 
-    for s in d['participant'].unique():
-        for c in d['Congruency'].unique():
-
+    for s in d["participant"].unique():
+        for c in d["Congruency"].unique():
             print(s, c)
 
-            dd = d.loc[(d['participant'] == s) & (d['Congruency'] == c)]
+            dd = d.loc[(d["participant"] == s) & (d["Congruency"] == c)]
 
             n_trials = dd.shape[0]
 
-            r_choice_obj = dd['Correct'].to_numpy()
-            r_time_obj = dd['RT'].to_numpy()
+            r_choice_obj = dd["Correct"].to_numpy()
+            r_time_obj = dd["RT"].to_numpy()
             args = (r_choice_obj, r_time_obj, n_trials)
 
             # TODO: choose reasonable values for bounds
@@ -120,20 +115,20 @@ def fit_model():
                 polish=False,
                 # updating='deferred',
                 # workers=-1,
-                disp=True)
+                disp=True,
+            )
 
             print(result.x, result.fun)
 
-            fout = '../fits/ppt_' + str(s) + '_congruency_' + c + '.txt'
-            with open(fout, 'w') as f:
-                tmp = np.concatenate((result['x'], [result['fun']]))
+            fout = "../fits/ppt_" + str(s) + "_congruency_" + c + ".txt"
+            with open(fout, "w") as f:
+                tmp = np.concatenate((result["x"], [result["fun"]]))
                 tmp = np.reshape(tmp, (tmp.shape[0], 1))
-                np.savetxt(f, tmp.T, '%0.4f', delimiter=',', newline='\n')
+                np.savetxt(f, tmp.T, "%0.4f", delimiter=",", newline="\n")
 
 
 def inspect_fits():
-
-    dir_fit = '../fits/'
+    dir_fit = "../fits/"
 
     v_mean = []
     v_sd = []
@@ -143,8 +138,8 @@ def inspect_fits():
     params = []
     condition = []
     for f in os.listdir(dir_fit):
-        if f.endswith('.txt'):
-            d = np.loadtxt(dir_fit + f, delimiter=',')
+        if f.endswith(".txt"):
+            d = np.loadtxt(dir_fit + f, delimiter=",")
             v_mean.append(d[0])
             v_sd.append(d[1])
             a.append(d[2])
